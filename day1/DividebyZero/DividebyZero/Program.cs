294d91093de46996dc9f633078b321a8fbd916a0
CsKriszta93 <kriszta.csere93@gmail.com>
2017-09-19 13:19:39 +0200

    first commit

---
+++ day1/DividebyZero/DividebyZero/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DividebyZero
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // create a function that takes a number
+            // divides ten with it,
+            // and prints the result.
+            // it should print "fail" if the parameter is 0
+            try
+            {
+                int diviser
+            }
+        }
+    }
+    public static double Divide(int input)
+    {
+        int ten = 10;
+        double number = ten / input;
+        return number;
+
+    }
+}
