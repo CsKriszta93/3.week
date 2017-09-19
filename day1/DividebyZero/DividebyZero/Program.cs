ebd433d1a4bab36e14a5479d34d789f822282707
CsKriszta93 <kriszta.csere93@gmail.com>
2017-09-19 17:17:20 +0200

    Create divide funtion

--- day1/DividebyZero/DividebyZero/Program.cs
+++ day1/DividebyZero/DividebyZero/Program.cs
@@ -8,23 +8,25 @@ namespace DividebyZero
 {
     class Program
     {
+        public static double Divide(int input)
+        {
+            double result = 10 / input;
+            if (input == 0)
+            {
+                Console.WriteLine("fail");
+            }
+            return result;
+        }
+
         static void Main(string[] args)
         {
             // create a function that takes a number
             // divides ten with it,
             // and prints the result.
             // it should print "fail" if the parameter is 0
-            try
-            {
-                int diviser
-            }
+            Console.WriteLine(Divide(5));
+            Console.ReadLine();
         }
     }
-    public static double Divide(int input)
-    {
-        int ten = 10;
-        double number = ten / input;
-        return number;
-
-    }
+    
 }
