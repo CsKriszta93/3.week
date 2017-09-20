7919b01cc436aa5b3ef1005d10a7e54e5b17fb6d
CsKriszta93 <kriszta.csere93@gmail.com>
2017-09-20 15:51:27 +0200

    complete PositionSquare exercise

---
+++ day2/PositionSquare/PositionSquare/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using GreenFox;
+using System.Windows.Shapes;
+
+namespace PositionSquare
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var foxDraw = new FoxDraw(canvas);
+            foxDraw.BackgroundColor(Colors.Linen);
+            // create a square drawing function that takes 2 parameters:
+            // the x and y coordinates of the square's top left corner
+            // and draws a 50x50 square from that point.
+            // draw 3 squares with that function. 
+            foxDraw.FillColor(Colors.DodgerBlue);
+            foxDraw.DrawSquare(45, 55);
+            foxDraw.DrawSquare(95, 95);
+            foxDraw.DrawSquare(145, 125);
+
+        }
+    }
+}
