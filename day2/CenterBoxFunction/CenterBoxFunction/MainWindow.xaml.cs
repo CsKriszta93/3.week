172abc17548bec06f76e986905dc4415592782b1
CsKriszta93 <kriszta.csere93@gmail.com>
2017-09-20 16:11:10 +0200

    Complete CenterBoxFunction

---
+++ day2/CenterBoxFunction/CenterBoxFunction/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using GreenFox;
+using System.Windows.Shapes;
+
+namespace CenterBoxFunction
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            InitializeComponent();
+            var foxDraw = new FoxDraw(canvas);
+            // create a square drawing function that takes 1 parameter:
+            // the square size
+            // and draws a square of that size to the center of the canvas.
+            // draw 3 squares with that function.           
+
+            foxDraw.FillColor(Colors.Goldenrod);
+            foxDraw.DrawSquare(210);
+           
+            foxDraw.FillColor(Colors.Maroon);
+            foxDraw.DrawSquare(140);
+            
+            foxDraw.FillColor(Colors.Black);
+            foxDraw.DrawSquare(70);     
+
+        }
+    }
+}
