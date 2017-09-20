2270b4886e264ca994c519484f23ea31b5f83b67
CsKriszta93 <kriszta.csere93@gmail.com>
2017-09-20 14:23:51 +0200

    Complete Horizontalline exercise

---
+++ day2/HorizontalLines/HorizontalLines/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using GreenFox;
+using System.Windows.Shapes;
+
+namespace HorizontalLines
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var foxDraw = new FoxDraw(canvas);
+
+            foxDraw.LineDrawing(15, 25);
+            foxDraw.LineDrawing(45, 110);
+            foxDraw.LineDrawing(66, 76);
+
+        }
+    }
+}
