e0f70f576bdc52281ec6eac55a2fec73b2854b43
CsKriszta93 <kriszta.csere93@gmail.com>
2017-09-21 17:02:53 +0200

    complete SumDigit exercise

---
+++ day3/SumDigit/SumDigit/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumDigit
+{
+    class Program
+    {
+        public static int DigitSum(int n)
+        {
+            if (n != 0)
+            {
+                return n % 10 + DigitSum(n / 10); 
+            }
+            return 0;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DigitSum(123));
+            Console.ReadLine();
+        }
+    }
+}
