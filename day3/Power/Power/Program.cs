f65dafc92a9572fb43b624f22a03b0dcc6c7ec7b
CsKriszta93 <kriszta.csere93@gmail.com>
2017-09-21 20:41:38 +0200

    complete Power exercise

---
+++ day3/Power/Power/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Power
+{
+    class Program
+    {
+        public static int PowerN(int num, int n)
+        {
+            if (n != 0 && num != 0)
+            {
+                num * PowerN(num, n - 1);
+            }
+            return 1;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(PowerN(3, 5));
+            Console.ReadLine();
+        }
+    }
+}
