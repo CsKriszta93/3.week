43b67265eac9072b1d3e9cd1976bd1545fe207a1
CsKriszta93 <kriszta.csere93@gmail.com>
2017-09-21 16:36:35 +0200

    complete NumberAdder exercise

---
+++ day3/NumberAdder/NumberAdder/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberAdder
+{
+    class Program
+    {
+        public static int AddNumber(int n)
+        {
+            if (n > 0)
+            {
+                return n += AddNumber(n - 1);
+            }
+            return 0;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(AddNumber(3)); 
+            Console.ReadLine();
+        }
+    }
+}
