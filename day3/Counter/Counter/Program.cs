b81db0cecdaa78ed8e207bcb52d1616943334866
CsKriszta93 <kriszta.csere93@gmail.com>
2017-09-21 16:08:40 +0200

    complete Counter exercise

---
+++ day3/Counter/Counter/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Counter
+{
+    class Program
+    {
+        public static int Counter(int n)
+        {
+            Console.WriteLine(n);
+            if (n > 1)
+            {
+                return Counter(n - 1);
+            }
+            return 0;           
+        }
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Counter(6));
+            Console.ReadLine();
+        }
+    }
+}
