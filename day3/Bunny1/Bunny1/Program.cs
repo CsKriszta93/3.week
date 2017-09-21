522cc24afecf65d83fb25e39e0e32dee386690b5
CsKriszta93 <kriszta.csere93@gmail.com>
2017-09-21 21:23:01 +0200

    BunnyEars exercise complete

---
+++ day3/Bunny1/Bunny1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bunny1
+{
+    class Program
+    {
+        public static int BunnyEars(int n)
+        {
+            if (n != 0)
+            {
+                return 2 + BunnyEars(n - 1);
+            }
+            return 0;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(BunnyEars(159));
+            Console.ReadLine();
+        }
+    }
+}
