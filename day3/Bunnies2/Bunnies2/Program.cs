abacb8743f79e97509963e69d8c94585f963b0ee
CsKriszta93 <kriszta.csere93@gmail.com>
2017-09-22 09:04:09 +0200

    Complete Bunnies2 Exercise

---
+++ day3/Bunnies2/Bunnies2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bunnies2
+{
+    class Program
+    {
+        public static int BunnyEars(int n)
+        {
+            if (n == 0)
+            {
+                return 0;
+            }
+            if (n % 2 == 0 )
+            {
+                return 3 + BunnyEars(n - 1);
+            }
+            else
+            {
+                return 2 + BunnyEars(n - 1);
+            }    
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(BunnyEars(3));
+            Console.ReadLine();
+        }
+    }
+}
